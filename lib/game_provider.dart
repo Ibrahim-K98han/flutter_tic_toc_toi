33c743733dd0fdf39dc5b6631881bde5b2bbbf38
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-06 20:28:33 +0600

    variable declaration ok

--- lib/game_provider.dart
+++ lib/game_provider.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
+enum Turn{
+  PlayerOne, PlayerTwo
+}
 class GameProvider with ChangeNotifier{
-
+  Turn turn = Turn.PlayerOne;
+  int position = 1;
+  int _scoreOne = 0;
+  int _scoreTwo = 0;
+  String _mark = '';
+  bool _first = true;
+  bool _hasEnded = false;
+  String _status = '';
+  int moves = 0;
+  var winingCombination = [];
+  var _checkList = [
+    '','','',
+    '','','',
+    '','','',
+  ];
+  int get playerOneScore => _scoreOne;
+  int get playerTwoScore => _scoreTwo;
+  String get mark => _mark;
+  bool get inFirst => _first;
+  bool get isEnd => _hasEnded;
+  String get result => _status;
+  List<String> get cList => _checkList;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tic_toc_toi/game_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,16 +8,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => GameProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container(),
       ),
-      home: Container(),
     );
   }
 }
