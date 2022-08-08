9ebf8b5eb9b3ea320b5f3d6cd5b88a4463fdb2d7
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-08 09:15:05 +0600

    array index set

--- lib/game_provider.dart
+++ lib/game_provider.dart
@@ -26,4 +26,71 @@ class GameProvider with ChangeNotifier{
   bool get isEnd => _hasEnded;
   String get result => _status;
   List<String> get cList => _checkList;
+
+  void setMark(String m){
+    _mark = m;
+    notifyListeners();
+  }
+  void setFirst(bool f){
+    _first = f;
+    notifyListeners();
+  }
+  void resetGame(){
+    _checkList = List.generate(9, (index) => '');
+    _hasEnded = false;
+    _mark = '';
+    _status = '';
+    moves = 0;
+    winingCombination = [];
+    turn = turn == Turn.PlayerOne ? Turn.PlayerTwo : Turn.PlayerOne;
+    position = turn == Turn.PlayerOne ? 1 : 2;
+    notifyListeners();
+  }
+  void checkWinner(){
+    if(moves > 4){
+      if(_checkList[0] == _mark && _checkList[1] == _mark && _checkList[2] == _mark){
+        winingCombination = [0,1,2];
+        endGame();
+        return;
+      }
+      if(_checkList[3] == _mark && _checkList[4] == _mark && _checkList[5] == _mark){
+        winingCombination = [3,4,5];
+        endGame();
+        return;
+      }
+      if(_checkList[6] == _mark && _checkList[7] == _mark && _checkList[8] == _mark){
+        winingCombination = [6,7,8];
+        endGame();
+        return;
+      }
+      if(_checkList[0] == _mark && _checkList[3] == _mark && _checkList[6] == _mark){
+        winingCombination = [0,3,6];
+        endGame();
+        return;
+      }
+      if(_checkList[1] == _mark && _checkList[4] == _mark && _checkList[7] == _mark){
+        winingCombination = [1,4,7];
+        endGame();
+        return;
+      }
+      if(_checkList[2] == _mark && _checkList[5] == _mark && _checkList[8] == _mark){
+        winingCombination = [0,1,2];
+        endGame();
+        return;
+      }
+      if(_checkList[0] == _mark && _checkList[4] == _mark && _checkList[8] == _mark){
+        winingCombination = [0,4,8];
+        endGame();
+        return;
+      }
+      if(_checkList[2] == _mark && _checkList[4] == _mark && _checkList[6] == _mark){
+        winingCombination = [2,4,6];
+        endGame();
+        return;
+      }
+    }
+  }
+  void endGame(){
+
+  }
 }
